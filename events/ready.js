7e7bbbe4848aa76662c9b1fa3bf16920d347bca0
KawaiiAlex <38259844+KawaiiAlex@users.noreply.github.com>
2018-10-23 15:11:46 +0200

    Add files via upload

--- events/ready.js
+++ events/ready.js
@@ -1,14 +1,15 @@
 const chalk = require('chalk');
 const Discord = require('discord.js');
+const fs = require("fs");
+const config = require('../config.json')
 const purplecolor = chalk.keyword('purple');
 
 module.exports = client => { // eslint-disable-line no-unused-vars
   console.log(purplecolor(`${client.user.username} en ligne. \n` + 'Connecté dans:\n' + client.guilds.array()));
- client.user.setActivity("<<help pour plus d'info")
-  client.user.setAvatar("https://cdn.discordapp.com/attachments/451811903950553098/451817237674852373/Capture.PNG");
-  /*
-  client.user.setActivity(`${client.users.username} en modification`, {type: "WATCHING"});
-  client.user.setStatus( 'idle' );
-  */
+ //client.user.setGame('//help pour plus d\'info', 'https://www.twitch.tv/ryvalgaming')//stream
+  //client.user.setAvatar("https://cdn.discordapp.com/attachments/458353530038911007/472502871326326785/unknown.png");//avatar
+  
+  client.user.setActivity(`//help`, {type: "WATCHING"});
+  
 
 };
