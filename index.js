3e035bf7728f5737b1eeefdf1bb6584fee0c3cb5
KawaiiAlex <38259844+KawaiiAlex@users.noreply.github.com>
2018-11-24 17:14:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -137,7 +137,7 @@ client.on('guildMemberAdd', async member => {
 	ctx.fillText('Welcome to the server,', canvas.width / 2.5, canvas.height / 3.5);
 
 	// Add an exclamation point here and below
-	ctx.font = await Canvas.applyText(canvas, `${member.displayName}!`);
+	ctx.font = '22px sans-serif'
 	ctx.fillStyle = '#ffffff';
 	ctx.fillText(`${member.displayName}!`, canvas.width / 2.5, canvas.height / 1.8);
 
